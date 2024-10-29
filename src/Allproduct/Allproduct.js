e78dbd38996df44e409aa2f84e4c4b042355b4de
Esther <jolugbatinuade@gmail.com>
2024-10-29 07:56:09 +0100

    intitai commit

--- src/Allproduct/Allproduct.js
+++ src/Allproduct/Allproduct.js
@@ -2,6 +2,17 @@ import React from 'react'
 import './Allproduct.css'
 import Camon from '../Assets/3.c7785c796d0b2bf5aa2b.png'
 import Cart from '../Assets/shopping_cart_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png'
+import charger from '../Assets/D7A7DA95-AEF8-228B-A2D2-A3FEBF237C33_y9p6wq.png'
+import Motor from '../Assets/MotoGPowerDual_2021_Reformatted_1_330x_wp8gve.png'
+import Product1 from '../Assets/acc.8c7894ef44f31a47cf12.png'
+import Product2 from '../Assets/acc2.e1fd1bb867bf154fc738.png'
+import Product3 from '../Assets/hm1.5c5c03cd5ca6b2c9b1ee.png'
+import Product4 from '../Assets/hm2.521dc183847c66dd7097.png'
+import Product5 from '../Assets/hm3.7d1bc536261889109962.jpg'
+import Product6 from '../Assets/laptop2.c6b05655a558214ed823.png'
+import Product7 from '../Assets/nikee.b2b62cd6973639eb0892.png'
+import Product8 from '../Assets/pro4.37aabec9b74d6573f22e.png'
+
 
 const Allproduct = () => {
   return (
@@ -9,6 +20,192 @@ const Allproduct = () => {
     <section id='Product-container'>
         
         <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Camon } alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Camoon Camera</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">7000 Naira</p> 
+
+        <del className='AmountFixed'>
+            9000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Product8 } alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>SwadTims</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">6000 Naira</p> 
+
+        <del className='AmountFixed'>
+            8000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Product1 } alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Intex Original</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">5000 Naira</p> 
+
+        <del className='AmountFixed'>
+            7000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Product3 } alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Spain Verse</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">9000 Naira</p> 
+
+        <del className='AmountFixed'>
+            19000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Product4} alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Cameroon Chair</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">170,000 Naira</p> 
+
+        <del className='AmountFixed'>
+            200,0000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Product2 } alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Samsung clip</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">4500 Naira</p> 
+
+        <del className='AmountFixed'>
+            5500 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Product5 } alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Spicy Cube</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">6000 Naira</p> 
+
+        <del className='AmountFixed'>
+            10,000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+    <div className='Product-mini-container'>
+        
+        <div className='ProductImg'>
+        <img src={Motor} alt="Camon "/>
+        </div>
+        <div class="imgText">
+            <h3>Camoon Camera</h3>
+        </div> 
+
+        <div class="Price-Container">
+        <p class="ProductAmount">7000 Naira</p> 
+
+        <del className='AmountFixed'>
+            9000 Naira
+            </del>
+        </div> 
+
+        <button> 
+            <img src={Cart} className='cartbtn' alt="Cart"/> 
+             Add to cart
+        </button>
+    </div>
+
+
+    <div className='Product-mini-container'>
         
         <div className='ProductImg'>
         <img src={Camon } alt="Camon "/>
@@ -52,22 +249,22 @@ const Allproduct = () => {
             <img src={Cart} className='cartbtn' alt="Cart"/> 
              Add to cart
         </button>
-        </div>
+    </div> 
 
-        <div className='Product-mini-container'>
+    <div className='Product-mini-container'>
         
         <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
+        <img src={Motor} alt="Camon "/>
         </div>
         <div class="imgText">
             <h3>Camoon Camera</h3>
         </div> 
 
         <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
+        <p class="ProductAmount">27000 Naira</p> 
 
         <del className='AmountFixed'>
-            9000 Naira
+            39000 Naira
             </del>
         </div> 
 
@@ -75,22 +272,22 @@ const Allproduct = () => {
             <img src={Cart} className='cartbtn' alt="Cart"/> 
              Add to cart
         </button>
-        </div>
+    </div>
 
-        <div className='Product-mini-container'>
+    <div className='Product-mini-container'>
         
         <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
+        <img src={Product7 } alt="Camon "/>
         </div>
         <div class="imgText">
-            <h3>Camoon Camera</h3>
+            <h3>AirMax</h3>
         </div> 
 
         <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
+        <p class="ProductAmount">70,000 Naira</p> 
 
         <del className='AmountFixed'>
-            9000 Naira
+            90,000 Naira
             </del>
         </div> 
 
@@ -98,22 +295,22 @@ const Allproduct = () => {
             <img src={Cart} className='cartbtn' alt="Cart"/> 
              Add to cart
         </button>
-        </div>
+    </div>
 
-        <div className='Product-mini-container'>
+    <div className='Product-mini-container'>
         
         <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
+        <img src={Product6} alt="Camon "/>
         </div>
         <div class="imgText">
-            <h3>Camoon Camera</h3>
+            <h3>lenovo</h3>
         </div> 
 
         <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
+        <p class="ProductAmount">500,0000 Naira</p> 
 
         <del className='AmountFixed'>
-            9000 Naira
+            1,000,000 Naira
             </del>
         </div> 
 
@@ -121,241 +318,9 @@ const Allproduct = () => {
             <img src={Cart} className='cartbtn' alt="Cart"/> 
              Add to cart
         </button>
-        </div>
+    </div>  
 
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-        <div className='Product-mini-container'>
-        
-        <div className='ProductImg'>
-        <img src={Camon } alt="Camon "/>
-        </div>
-        <div class="imgText">
-            <h3>Camoon Camera</h3>
-        </div> 
-
-        <div class="Price-Container">
-        <p class="ProductAmount">7000 Naira</p> 
-
-        <del className='AmountFixed'>
-            9000 Naira
-            </del>
-        </div> 
-
-        <button> 
-            <img src={Cart} className='cartbtn' alt="Cart"/> 
-             Add to cart
-        </button>
-        </div>
-
-
-
-
+    
 
 
 
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
     <section id='Navbar-container'>
-        <div className=''>
-            <ul>
-            <li><a href="#home">All Products</a></li>
-            <li><a href="#news">Laptops</a></li>
-            <li><a href="#contact">Clothes</a></li>
-            <li><a href="#about">Accessories Solar</a></li>
-            <li><a href="#about">Phones & Tablets</a></li>
-           </ul>
-        </div>
-
+       
+          
+            <Link activeClass='active' to='Product-container' spy={true} smooth={true} offset={-100} duration={500} className='navList'>All Products</Link>
+            <Link activeClass='active' to='Allproduct' spy={true} smooth={true} offset={-100} duration={500} className='navList' >Laptops</Link>
+            <Link activeClass='active' to='Allproduct' spy={true} smooth={true} offset={-100} duration={500} className='navList' >Clothes</Link>
+            <Link activeClass='active' to='Allproduct' spy={true} smooth={true} offset={-100} duration={500} className='navList' >Accessories Solar</Link>
+            <Link activeClass='active' to='Allproduct' spy={true} smooth={true} offset={-100} duration={500} className='navList' >Phones & Tablets</Link>
+        
     </section>
   )
 }
